90ca464e893d189bd2bc7d28ae49ee8befb135d4
Miguel Fuentes <miguel@wemarket.com>
2014-05-19 13:13:11 +0200

    Miguel Commit: ma 19 mei 2014 13:13:11 CEST

--- src/animation.js
+++ src/animation.js
@@ -8,6 +8,7 @@ Saga.Animation = (function () {
         debug = Saga.Debug,
         animations = [],
         animationCore,
+        prefix = ['-webkit-', '-moz-', '-o-', '-ms-', ''],
         transitionTypes = ['-webkit-transition', '-moz-transition', '-o-transition', '-ms-transition', 'transition'],
         transformTypes = ['-webkit-transform', '-moz-transform', '-o-transform', '-ms-transform', 'transform'],
         transformTemplate = "",
@@ -40,6 +41,9 @@ Saga.Animation = (function () {
             "perspective",
             "initial",
             "inherit"
+        ],
+        noCamel = [
+            "transform-origin"
         ],
         toCamelCase = function (str) {
             if (str.charAt(0) === "-") {
@@ -95,42 +99,59 @@ Saga.Animation = (function () {
             });
             return styles;
         },
-
-
         transformStyles = function (props, elem) {
             var transformStyles = {},
                 styles = {},
-                //prevTransform = {},
                 tmp;
 
             if (elem) { //&& elem.hasOwnProperty("style") && elem.style.hasOwnProperty("transform")) {
-                //debug.error("tStyles elem: ", elem, elem.style.transform);
                 if (elem.style.transform !== "") {
                     u.each(elem.style.transform.split(" "), function (val) {
                         tmp = val.split("(");
-                        //debug.warn("tmp", tmp);
                         transformStyles[tmp[0]] = val;
                     });
                 }
             }
-            //debug.error("1", transformStyles);
-            // need previous string!!!!
-            /*
-            if (!props.hasOwnProperty("translate3d")) { // force 3d // wont work!, its a string in the value!!
-                props.translate3d = "0, 0, 0";
+            if (!transformStyles.hasOwnProperty("translate3d")) { // force 3d 
+                transformStyles.translate3d = "translate3d(0, 0, 0)";
             }
-            */
             u.each(props, function (val, prop) {
                 if (transformProperties.hasOwnProperty(prop)) {
-                    //transformStyles.push(prop + "(" + val + ")");
                     transformStyles[prop] = prop + "(" + val + ")";
                 } else {
-                    styles[toCamelCase(prop)] = val;
+                    /*
+                    if (!noCamel.hasOwnProperty(prop)) {
+                        prop = toCamelCase(prop);
+                    }
+                    */
+                    if (prop === "transform-origin") {
+                        styles.webkitTransformOrigin = val;
+                        styles.MozTransformOrigin = val;
+                        styles.msTransformOrigin = val;
+                        styles.OTransformOrigin = val;
+                        styles.transformOrigin = val;
+                        
+                        /*
+                        elem.style.webkitTransformOrigin = "0px 0px";
+                        elem.style.MozTransformOrigin = "0px 0px";
+                        elem.style.msTransformOrigin = "0px 0px";
+                        elem.style.OTransformOrigin = "0px 0px";
+                        elem.style.transformOrigin = "0px 0px";
+                        */
+                        /*
+                        debug.error("transform-origin", prop, val)
+                        u.each(prefix, function (prfx) {
+                            styles[prfx + "transform-origin"] = val;
+                        });
+                        
+                        debug.error(styles);
+                        */
+                    } else {
+                        styles[prop] = val;
+                    }
                 }
             });
-            //debug.error("2", transformStyles);
             transformStyles = u.toArray(transformStyles).join(" ");
-            //debug.error("3", transformStyles);
             u.each(transformTypes, function (type) {
                 styles[type] = transformStyles;
             });
@@ -153,7 +174,7 @@ Saga.Animation = (function () {
             u.each(styles, function (value, style) {
 
                 elem.style[style] = value;
-                debug.info("Saga.Dom.setStyles() -> Applying: ", style, " -> ", value, elem.style[style]);
+                debug.info("Saga.Dom.setStyles() -> Applying: ", style, value);
             });
         },
         animationCss3 = function () {
@@ -165,10 +186,7 @@ Saga.Animation = (function () {
                 },
                 to = function (elem, props, time, cb) {
                     debug.warn("to", elem, props, time);
-                    /*
-                    -webkit-transition: width 2s, height 2s,-webkit-transform 2s;  // For Safari 3.1 to 6.0 
-                    transition: width 2s, height 2s, transform 2s;
-                    */
+
                     var transStyles = {},
                         tEndListener;
                     u.each(props, function (val, prop) {
@@ -195,82 +213,6 @@ Saga.Animation = (function () {
                     u.defer(function () {
                         setStyles(elem, props);
                     });
-
-                    /*
-                    
-                    if (transitionEvts) {
-                        logoContainer.addEventListener(transitionEvts.end, function (something) {
-                            debug.log("App.Manager.initComplete() -> Animation End ", something, logoContainer, transitionEvts.end);
-                            if (ended === 0) {
-                                if (cb) {
-                                    cb();
-                                    cb = null;
-                                }
-                            }
-                            ended += 1;
-                        });
-                    } else {
-                        if (cb) {
-                            cb();
-                            cb = null;
-                        }
-                    }
-                    d.setStyles(logoContainer, transform);
-                }, 100);
-                    */
-
-
-                    /*
-                    var newS = u.object(u.map(props, function (value, prop) {
-                            return [prop, time + "ms"];
-                        })),
-                        transStyles = transitionStyles(newS);
-
-                    debug.log(newS, transStyles);
-
-                    setStyles(elem, transStyles);
-                    u.defer(function () {
-                        setStyles(elem, props);
-                    });
-                    */
-
-                    /*
-                    we get 
-                    props:
-                    {
-                        'margin-top':"auto",
-                        'width':"100px"
-                    }
-                    
-                    time: 100 (ms)
-                    
-                    function needs : 
-                    {
-                        transform: "1s",
-                        opacity: "0.4s",
-                        'margin-top': "0.5s"
-                    }
-                    
-                    */
-
-                    // chec for transform styles
-                    /*
-                    debug.warn("to: ", props);
-                    var newS = u.object(u.map(props, function (value, prop) {
-                            return [prop, time + "ms"];
-                        })),
-                        transStyles = transitionStyles(newS);
-
-                    debug.warn("to", elem, newS, transStyles, transformStyles(props));
-                    set(elem, newS);
-                    */
-                    /*
-                    u.delay(function () {
-                        setStyles(elem, transformStyles({
-                            scale: 2
-                        }));
-                    }, 10);
-                    */
                 };
 
             pub = {
@@ -285,25 +227,12 @@ Saga.Animation = (function () {
             return pub;
         };
 
-    /*
-    u.each(transformTypes, function (type) {
-        transformTemplate[type]="[TRANSFORM]";
-    });
-    //debug.warn(template);
-    transformTemplate = u.template(transformTemplate);
-*/
-
+    noCamel = u.object(noCamel, noCamel);
     transformProperties = u.object(transformProperties, transformProperties);
     animationCore = animationCss3();
 
     pub = {
-        /*
-        gen: function (elem, styles) {
-            return animationCss3(elem, styles);
-        }
-        */
         set: function () {
-            //animationCore.set();
             animationCore.set.apply(this, arguments);
         },
         to: function () {
