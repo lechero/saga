ba5836b3b9893686f3ee6b158e894d544881bcc3
Miguel Fuentes <miguel@wemarket.com>
2014-09-16 18:41:03 +0200

    Miguel Commit: di 16 sep 2014 18:41:03 CEST

--- example.old/public_html/app/js/about.js
+++
@@ -1,8 +0,0 @@
-/*jslint browser:true*/
-/*global App, Saga*/
-
-(function (asset) {
-    "use strict";
-    var view = asset.View;
-    Saga.Util.extend(view, App.SimpleView(asset));
-}(App.Assets.About));
